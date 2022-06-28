c4a2b0a74f6bcc179c09fd730e56fba2e1e15500
Gabriel <gabrieltsf10@gmail.com>
2022-06-28 11:59:07 -0300

    Cadastro com hooks e função esconder senha

--- figma-clone/components/cadastro/Cadastro.js
+++ figma-clone/components/cadastro/Cadastro.js
@@ -44,7 +44,7 @@ export const Cadastro = () => {
           onChangeText={text => setSenha(text)} />
           <Pressable onPress={handlePasswordVisibility}>
           
-        </Pressable>
+          </Pressable>
           <BotaoCadastrar>
             <TextoCadastrar>CADASTRAR</TextoCadastrar>
           </BotaoCadastrar>
