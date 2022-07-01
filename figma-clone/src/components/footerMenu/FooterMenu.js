1a551d449599464333c1d0c35cb146f27a2be94e
isabeels <101607831+isabeels@users.noreply.github.com>
2022-07-01 17:04:42 -0300

    Deletando imports nao utilizados

--- figma-clone/src/components/footerMenu/FooterMenu.js
+++ figma-clone/src/components/footerMenu/FooterMenu.js
@@ -1,23 +1,26 @@
 import React from "react";
-import { Container } from "./styles"
-import { View, Text, Image } from "react-native";
-import { AntDesign, Feather,Ionicons, SimpleLineIcons } from '@expo/vector-icons';
-import styled from "styled-components/";
-
+import { Container } from "./styles";
+import { View } from "react-native";
+import {
+  AntDesign,
+  Feather,
+  Ionicons,
+  SimpleLineIcons,
+} from "@expo/vector-icons";
 
 export const Footer = (props) => {
-  let homeAtivo = props.homeAtivo ? 'blue' : 'grey';
-  let bagAtivo = props.bagAtivo ? 'blue' : 'grey';
-  let starAtivo = props.starAtivo ? 'blue' : 'grey';
-  let shoppingAtivo = props.shoppingAtivo ? 'blue' : 'grey';
+  let homeAtivo = props.homeAtivo ? "blue" : "grey";
+  let bagAtivo = props.bagAtivo ? "blue" : "grey";
+  let starAtivo = props.starAtivo ? "blue" : "grey";
+  let shoppingAtivo = props.shoppingAtivo ? "blue" : "grey";
   return (
-    <View >
+    <View>
       <Container>
-      <Ionicons name="home-outline" size={24} color={homeAtivo}/>
-      <SimpleLineIcons name="bag" size={24} color={bagAtivo} />
-      <Feather name="star" size={24} color={starAtivo} />
-      
-      <AntDesign name="shoppingcart" size={24} color={shoppingAtivo} />
+        <Ionicons name="home-outline" size={24} color={homeAtivo} />
+        <SimpleLineIcons name="bag" size={24} color={bagAtivo} />
+        <Feather name="star" size={24} color={starAtivo} />
+
+        <AntDesign name="shoppingcart" size={24} color={shoppingAtivo} />
       </Container>
     </View>
   );
