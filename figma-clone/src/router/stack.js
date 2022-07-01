0ed647d05e687dd1bb281d8f1950286b4ad21d45
isabeels <101607831+isabeels@users.noreply.github.com>
2022-07-01 16:46:42 -0300

    Criando as rotas

---
+++ figma-clone/src/router/stack.js
@@ -0,0 +1,68 @@
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import Login from "./../screens/login/Login";
+import CriaSenha from "./../screens/criaSenha/Index";
+import RecuperarSenha from "./../screens/recuperaSenha/RecuperarSenha";
+import ConfirmaSenha from "./../screens/confirmaSenha/index";
+import { Cadastro } from "./../screens/cadastroUsuario/Cadastro";
+import ConfirmaCadastro from "./../screens/confirmaCadastro/index";
+import Marketplace from "./../screens/marketplace/Marketplace";
+
+const { Screen, Navigator } = createNativeStackNavigator();
+
+export function Stack() {
+  return (
+    <Navigator>
+      <Screen
+        name="Login"
+        component={Login}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Screen
+        name="Cadastro"
+        component={Cadastro}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Screen
+        name="RecuperarSenha"
+        component={RecuperarSenha}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Screen
+        name="CriaSenha"
+        component={CriaSenha}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Screen
+        name="ConfirmaSenha"
+        component={ConfirmaSenha}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Screen
+        name="ConfirmaCadastro"
+        component={ConfirmaCadastro}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Screen
+        name="Marketplace"
+        component={Marketplace}
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Navigator>
+  );
+}
