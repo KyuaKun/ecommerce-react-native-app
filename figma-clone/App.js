2d1f57a937b441e2775f3a875a5e1a82803141cc
isabeels <101607831+isabeels@users.noreply.github.com>
2022-07-03 23:36:11 -0300

    Remove import nao utilizado

--- figma-clone/App.js
+++ figma-clone/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes } from "./src/router";
-import {Splash} from "./src/screens/splash/index";
+import { Splash } from "./src/screens/splash/index";
 
 export default function App() {
   return (
